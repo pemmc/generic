dce97f5c3ebafc5e37ef797d5ac7feaf2b6e520d
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-22 11:33:09 -0300

    204. Generics PARTE 1

---
+++ Generics1/PrintService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Generics1
+{
+    public class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void addValue(int value)
+        {
+            if(_count ==10)
+            {
+                throw new InvalidOperationException("Vetor cheio");
+            }
+
+            _values[_count] = value;
+            _count++;
+
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Vetor ainda vazio, não existe a posição zero");
+            }
+
+            return _values[0];
+
+        }
+
+        public void print()
+        {
+
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Vetor ainda vazio, não existem elementos");
+
+            }
+
+            Console.Write("[");
+
+
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+
+            }
+
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+
+            }
+
+        Console.Write("]");
+
+        }
+
+    }
+}
---
+++ Generics1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("Quanto valores? ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i<n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                printService.addValue(x);
+
+            }
+
+
+            printService.print();
+            Console.WriteLine();
+
+            Console.WriteLine("First: " + printService.First());
+           
+        }
+    }
+}
